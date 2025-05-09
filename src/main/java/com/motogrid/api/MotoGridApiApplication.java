14c280c5f8ba455dca7178a1b16bc9c04e1b6b66
GomesMancera <manceragomesbr@gmail.com>
2025-05-09 11:43:01 -0300

    🟢 Projeto MotoGridAPI com estrutura inicial e README

---
+++ src/main/java/com/motogrid/api/MotoGridApiApplication.java
@@ -0,0 +1,13 @@
+package com.motogrid.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoGridApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoGridApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motogrid/api/MotoGridApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motogrid.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoGridApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
